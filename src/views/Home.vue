43fa27a7cdb0468b6227f1b6aaddd02beee838f5
Daria <daryalutkova@yandex.ru>
2019-11-30 22:36:43 +0300

    redesigned home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-    <h3>{{$store.getters.getCount}}</h3>
-    <button @click="$store.commit('addCount', 10)" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-      Add
-    </button>
+  <div id="main">
+    <h1>Microservices</h1>
+    <h4>Custom solution for API<br> information monitoring.</h4>
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">Start now</button>
+    <img src="../assets/bg.png">
   </div>
 </template>
 <script>
@@ -12,6 +11,21 @@
     name: 'Home'
   }
 </script>
-<style>
-  
+<style scoped>
+  #main{
+    height: 80vh;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: baseline;
+  }
+
+  #main h1{
+    padding-top: 100px;
+  }
+  #main img{
+    position: absolute;
+    width: 1000px;
+    top: 0px;
+    right: 20px;
+  }
 </style>
